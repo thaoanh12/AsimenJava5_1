8699329da2151db3cd54afe107c3da0d7bdd75df
thaoanh12 <anhduongthao2002@gmail.com>
2023-10-07 13:49:31 +0700

    update

---
+++ src/main/java/com/example/demo/AsimenJava5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsimenJava5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsimenJava5Application.class, args);
+    }
+
+}
